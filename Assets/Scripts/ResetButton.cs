54ef998a59974bd76da263d41633b388834cadf9
preischadt <lucas_p@outlook.com>
2020-02-01 10:21:11 -0300

    Spring-like movement

---
+++ Assets/Scripts/ResetButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetButton : MonoBehaviour
+{
+    private Vector3 initialPosition;
+    public Rigidbody target;
+
+    private void Awake()
+    {
+        initialPosition = target.position;
+    }
+
+    public void OnPress()
+    {
+        Debug.Log($"Reseting to position: {initialPosition}");
+        target.velocity = Vector3.zero;
+        target.position = initialPosition;
+    }
+}

8d5b1eb904d004f029aacc542e6227550d315263
Murilo <murilo.fioravanti.costa@gmail.com>
2020-02-01 16:43:30 -0300

    Coisas do murilo

---
+++ Assets/Scripts/MoveC.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveC : MonoBehaviour
+{
+    private bool gyroEnabled;
+    private Gyroscope gyro;
+    private GameObject GyroControl;
+    private Quaternion rot;
+    private Quaternion adjustrot;
+    private Vector3 originalPos;
+
+    private void Start()
+    {
+        GyroControl = new GameObject("Gyro Control");
+        GyroControl.transform.position = transform.position;
+        transform.SetParent(GyroControl.transform);
+        gyroEnabled = EnableGyro();
+        adjustrot = Quaternion.Euler(90f, 0f, 0f) * Quaternion.Inverse(gyro.attitude);
+        originalPos = transform.position;
+    }
+
+
+    private bool EnableGyro()
+    {
+        if (SystemInfo.supportsGyroscope)
+        {
+            gyro = Input.gyro;
+            gyro.enabled = true;
+            GyroControl.transform.rotation = Quaternion.Euler(90f, -90f, 0f);
+            rot = new Quaternion(0, 0, 1, 0);
+            return true;
+        }
+        return false;
+    }
+
+    private void Update()
+    {
+        if (gyroEnabled)
+        {
+            if (Input.touchCount == 3)
+            {
+                adjustrot = Quaternion.Euler(90f, 0f, 0f) * Quaternion.Inverse(gyro.attitude);
+                transform.position = originalPos;
+            }
+            transform.localRotation = adjustrot * gyro.attitude * rot;
+            var speed = Input.gyro.userAcceleration.z * 10f;
+            transform.position += transform.forward * speed * Time.deltaTime;
+        }
+    }
+}
---
+++ Assets/Scripts/SwingTrack.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class SwingTrack : MonoBehaviour
+{
+    bool isMoving = false;
+
+    float threshold = 1f;
+
+    float totalForce = 0f;
+
+    float lastForce = 0f;
+
+    public TMP_Text textMesh;
+
+    public Rigidbody hammer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //textMesh = FindObjectOfType<TMP_Text>();
+        Input.gyro.enabled = true;
+        Handheld.Vibrate();
+        Debug.Log(hammer.maxAngularVelocity);
+        hammer.maxAngularVelocity = 100f;
+    }
+
+    float speed = 10f;
+    float smooth = 1f;
+
+    Vector2 rotation;
+
+    void OnMouseDrag()
+    {
+        rotation.y += Input.GetAxis("Mouse X") * speed;
+        rotation.x += -Input.GetAxis("Mouse Y") * speed;
+
+        var baseRotation = Quaternion.Euler(rotation.x, rotation.y, 0);
+        transform.rotation = Quaternion.Lerp(transform.rotation, baseRotation, smooth);
+
+        GetComponent<Rigidbody>().velocity = Vector3.zero;
+        GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+    }
+
+    void FixedUpdate()
+    {
+        if(Input.touchCount >= 3)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            isMoving = true;
+            totalForce = 10f;
+        }
+        var currentForce = Input.gyro.userAcceleration.x * 1f;
+
+        if (isMoving)
+        {
+            if (currentForce < threshold)
+            {
+                isMoving = false;
+                lastForce = totalForce;
+                hammer.AddRelativeTorque(0f, 0f, totalForce * 100f);
+
+                Debug.Log("Last Force: " + totalForce);
+                Vibration.CreateOneShot(200, Mathf.Min((int)totalForce * 10, 255));
+            }
+            else
+            {
+                totalForce += currentForce;
+            }
+        }
+        else
+        {
+            isMoving = currentForce > threshold;
+            totalForce = currentForce;
+        }
+
+        textMesh.text = "Last Force: " + lastForce;
+    }
+}
